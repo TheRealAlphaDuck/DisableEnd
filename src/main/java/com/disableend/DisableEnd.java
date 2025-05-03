d75e4782c5167b040975a814ec12d1e1f428ece3
zach <zachbanks777@gmail.com>
2025-05-02 22:16:59 -0400

    1.16 changes

--- src/main/java/com/disableend/DisableEnd.java
+++ src/main/java/com/disableend/DisableEnd.java
@@ -5,8 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
 import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class DisableEnd implements ModInitializer {
 	public static final String MOD_ID = "disable_end";
@@ -14,7 +12,6 @@ public class DisableEnd implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final GameRules.Key<GameRules.BooleanRule> DISABLE_END = GameRuleRegistry.register("disableEnd", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
 
 
--- src/main/java/com/disableend/mixin/DisableEndPortal.java
+++ src/main/java/com/disableend/mixin/DisableEndPortal.java
@@ -4,7 +4,6 @@ import com.disableend.DisableEnd;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.EndPortalBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCollisionHandler;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,7 +16,7 @@ import java.util.Objects;
 @Mixin(EndPortalBlock.class)
 public class DisableEndPortal {
     @Inject(method = "onEntityCollision", at = @At("HEAD"), cancellable = true)
-    private void noEnd(BlockState state, World world, BlockPos pos, Entity entity, EntityCollisionHandler handler, CallbackInfo ci){
+    private void noEnd(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci){
         if (Objects.requireNonNull(world.getServer()).getGameRules().getBoolean(DisableEnd.DISABLE_END)) {
             ci.cancel();
         }
